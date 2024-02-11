f0a94388be881f1d449f40703c3ab0bdcf1079de
Pankaj Pawar <pankaj@Pankajs-MacBook-Air.local>
2024-02-12 01:26:33 +0530

    REST API with go fiber

---
+++ 22-restapi/main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+// create movieDB slice variable from Movies struct
+var movieDB []Movies
+
+// Movies struct
+type Movies struct {
+	Id          int      `json:"movie_number"`
+	Name        string   `json:"movie_name"`
+	Rating      float32  `json:"rating"`
+	Genres      []string `json:"genres"`
+	ReleaseDate string   `json:"movie_release_date"`
+	Poster      string   `json:"movie_banner"`
+}
+
+func main() {
+	fmt.Println("Design REST API In GoLang!")
+
+	app := fiber.New()
+
+	app.Get("/", getHomePath)
+
+	// Get list of movies from DB
+	app.Get("/movie/list", getMovieList)
+
+	// Insert Movie Into DB
+	app.Post("/movie/list", insertMovie)
+
+	// Delete Movie From DB
+	app.Delete("/movie/:id", deleteMovie)
+
+	app.Listen(":3000")
+}
+
+func getHomePath(c *fiber.Ctx) error {
+	return c.SendString("Welcome to movies library!")
+}
+
+func insertMovie(c *fiber.Ctx) error {
+	var parsedMovie Movies
+	if err := c.BodyParser(&parsedMovie); err != nil {
+		panic(err)
+	}
+	movieDB = append(movieDB, parsedMovie)
+	return c.JSON(movieDB)
+}
+
+func getMovieList(c *fiber.Ctx) error {
+	// Generate Dummy Data
+	// moviesData := []Movies{
+	// 	{Name: "Terminater 1", Rating: 5, Genres: []string{"action", "animation"}, ReleaseDate: "2024-02-11", Poster: "https://m.media-amazon.com/images/I/71+Nertw2+S._SY741_.jpg"},
+	// }
+	// return c.JSON(moviesData)
+
+	// Return DB Values
+	return c.JSON(movieDB)
+}
+
+func deleteMovie(c *fiber.Ctx) error {
+	parameters := c.AllParams()
+	movieID, _ := strconv.Atoi(parameters["id"])
+	for key, data := range movieDB {
+		if data.Id == movieID {
+			movieDB = append(movieDB[:key], movieDB[key+1:]...)
+		}
+	}
+
+	return c.JSON(movieDB)
+}
+
+/*
+Use Below JSON as Request Body to insert Movies
+
+{
+    "movie_number": 1,
+    "movie_name": "Terminater 1",
+    "rating": 5,
+    "genres": [
+        "action",
+        "animation"
+    ],
+    "movie_release_date": "2024-02-11",
+    "movie_banner": "https://m.media-amazon.com/images/I/71+Nertw2+S._SY741_.jpg"
+}
+
+Do GET Movies
+DO DELETE Movies
+
+Play with it...
+*/
